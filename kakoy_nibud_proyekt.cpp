8fc4c66518678e9dc00ee026bfad58520e079446
pr0suwanted <prosuwanted@gmail.com>
2022-06-11 14:55:15 +0400

    The best C++ project

---
+++ kakoy_nibud_proyekt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<string> words = {"Hello", "Maximus", "I", "Love", "Mironov", "&", "Sakhno"};
+	for (int i=0; i<100; i++) cout << words[i % words.size()] << endl;	
+	return 0;
+}
