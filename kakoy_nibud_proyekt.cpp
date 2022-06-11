80334210973e2f6b1d1fb112f32ac6cabaf3c843
pr0suwanted <prosuwanted@gmail.com>
2022-06-11 15:13:44 +0400

    New Sakhno wishes

--- kakoy_nibud_proyekt.cpp
+++ kakoy_nibud_proyekt.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main() {
 	vector<string> words = {"Hello", "Maximus", "I", "Love", "Mironov", "&", "Sakhno"};
-	for (int i=0; i<100; i++) cout << words[i % words.size()] << endl;	
+	float kills = 1.2f;
+	for (int i=0; i<100; i++) {
+		cout << words[i % words.size()] << endl;
+		kills *= 1.742f;
+		cout << "I would kill Sakhno " << kills << " times!" << endl;
+	}
 	return 0;
 }
